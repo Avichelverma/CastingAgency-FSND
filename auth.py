632aa87def3683e6fafaaeff76147d5ca5296258
Avichel Verma <averma18@hawk.iit.edu>
2020-06-23 22:14:31 -0500

    fixing issue

--- auth.py
+++ auth.py
@@ -6,9 +6,9 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = os.getenv('AUTH0_DOMAIN')
-ALGORITHMS = [os.getenv('ALGORITHMS')]
-API_AUDIENCE = os.getenv('API_AUDIENCE')
+AUTH0_DOMAIN = 'fnsdlearning.us.auth0.com'
+ALGORITHMS = ['RS256']
+API_AUDIENCE = 'casting'
 
 # AuthError Exception
 
--- test_app.py
+++ test_app.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from app import create_app
 from models import setup_db, Movie, Actor
 
+assistant_token = "Bearer {}".format('eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImhzRVVvT0k3RTdZeFdCNE80Yk5zTyJ9.eyJpc3MiOiJodHRwczovL2Zuc2RsZWFybmluZy51cy5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NWVmMTE3Zjk3ZmIzZjAwMDEzZTRkMTA4IiwiYXVkIjoiY2FzdGluZyIsImlhdCI6MTU5Mjk2NzEwMiwiZXhwIjoxNTkyOTc0MzAyLCJhenAiOiJBc1p4Ym9ld2ZUeWlmOUtoNGlaRklYNVFCZWtyUmFJNiIsInNjb3BlIjoiIiwicGVybWlzc2lvbnMiOlsiZGVsZXRlOmFjdG9ycyIsImRlbGV0ZTptb3ZpZXMiLCJnZXQ6YWN0b3JzIiwiZ2V0Om1vdmllcyIsInBhdGNoOmFjdG9ycyIsInBhdGNoOm1vdmllcyIsInBvc3Q6YWN0b3JzIiwicG9zdDptb3ZpZXMiXX0.bLzL-JOKgtBz3YAO2KD2BGnzrLFo67kun7maQzgrFqTRCX9-fG7stAcxsPzc1aJQuJGr0U23a5go6W8iBf3TVeRfkgDesnyCe2c_o0I8yaG3KN3hm62WeVKmwf8ll3uaZwWIEHkgfk1qHyKTI4rzNIj19pZ0XUgkE90fV1b3Y9QI2H9a3eXNAYviBMz8obhspwDfSzFHvLuidpCKZh58EH5HQpP0LjWvT9Y8XJ7LJQz4QfeIOhapQYwC_2l8F0iMa-DQ0fvvUaI9MWKWwJ4g08CpbeH31enI-OCavZ2hAxl7WVWxVXN4jEtn-NRUWyVdM4YpzhqVNMxZpxZCx2SzqA')
 
 class CastingAgencyUnitTest(unittest.TestCase):
 
@@ -32,7 +33,7 @@ class CastingAgencyUnitTest(unittest.TestCase):
     def test_get_movies(self):
         print(self.casting_assistant)
         print("-----------------")
-        response = self.client().get('/movies', headers=self.casting_assistant)
+        response = self.client().get('/movies', headers={"Authorization": (assistant_token)})
         data = json.loads(response.data)
 
         self.assertEqual(response.status_code, 200)
